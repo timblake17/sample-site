dd8a1def5fe357cae4a2223d8e1e83fdcb27834a
Tim Blake <timblake17x@gmail.com>
2018-03-13 19:38:24 -0400

    movies and books filtering

--- src/components/movies.js
+++ src/components/movies.js
@@ -2,47 +2,57 @@ import React, { Component } from 'react';
 import data from './data.json';
 
 
+
 class Movies extends React.Component  {
   constructor(){
     super();
 
-    ///get initial state
-    this.state = {
-      show: 'movies'
-    };
+    this.state = {show:'movies'};
+
+    this.filterBooks = this.filterBooks.bind(this);
   }
+
+  filterBooks() {
+    this.setState({show:'books'})
+    console.log('books')
+  }
+
+  filterMovies() {
+    this.setState({show:'movies'})
+    console.log('hi')
+  }
+
   render() {
     return (
       <div>
-
-        <input type="radio" onClick= "#" name="movies" value="" />
-        <label>Movies</label>
-        <input type="radio" name="books" value="" />
-        <label>Books</label>
+        <input type="Checkbox" name="book" value="" onClick={(e) => this.filterBooks(e)} />
+        <label>Book</label>
+        <input type="Checkbox" name="movies" value="" onClick={(e) => this.filterMovies(e)}/>
+        <label>Movie</label>
 
         <ul>
           {
-            data.media.map(function(movie){
-            if (movie.type.includes('movie')) {
-            return <li>{movie.year} - {movie.title}-{movie.genre}
-            <img src={movie.poster} alt="" />
-            </li>;
+            data.media.map(movie=>{
+            if (movie.type.includes('book') && this.state.show ==='books') {
+            return <li key={movie.title}>{movie.title}-{movie.year}
+              <img src={movie.poster} alt="" />
+              </li>
             }
            })
           }
           {
-        data.media.map(function(movie){
-          if (movie.type.includes('book')) {
-          return <li>{movie.year} - {movie.title}-{movie.genre}
-            <img src={movie.poster} alt="" />
-          </li>;
-        }
-        })
-      }
+            data.media.map(movie=>{
+            if (movie.type.includes('movie') && this.state.show ==='movies') {
+            return <li key={movie.title}>{movie.title}-{movie.year}
+              <img src={movie.poster} alt="" />
+              </li>
+            }
+          })
+          }
       </ul>
     </div>
     )
   }
 }
 
-  export default Movies;
+export default Movies;
