516cd016ab44f2a726c1a54ab51667e8040e4291
Tim Blake <timblake17x@gmail.com>
2018-03-14 00:26:25 -0400

    genre filters working

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import data from './data.json';
+import './movies.css'
 
 
 
@@ -7,28 +8,72 @@ class Movies extends React.Component  {
   constructor(){
     super();
 
-    this.state = {show:'movies'};
+    this.state = {
+      show:'',
+      genre:''
 
-    this.filterBooks = this.filterBooks.bind(this);
+  };
   }
 
   filterBooks() {
     this.setState({show:'books'})
-    console.log('books')
   }
 
   filterMovies() {
     this.setState({show:'movies'})
-    console.log('hi')
   }
+
+  filterAction() {
+    this.setState({genre:'action'})
+  }
+
+  filterComedy() {
+    this.setState({genre:'comedy'})
+  }
+
+  filterAnimation() {
+    this.setState({genre:'animation'})
+  }
+
+  filterAdventure() {
+    this.setState({genre:'adventure'})
+
+  }
+
+  clearFilters() {
+    this.setState({show:''})
+    this.setState({genre:''})
+  }
+
+
+
+
 
   render() {
     return (
       <div>
-        <input type="Checkbox" name="book" value="" onClick={(e) => this.filterBooks(e)} />
-        <label>Book</label>
-        <input type="Checkbox" name="movies" value="" onClick={(e) => this.filterMovies(e)}/>
-        <label>Movie</label>
+        <div>
+          <input type="Checkbox" name="book" value="" onChange={(e) => this.filterBooks(e)} />
+          <label>Books</label>
+          <input type="Checkbox" name="movies" value="" onClick={(e) => this.filterMovies(e)}/>
+          <label>Movies</label>
+        </div>
+        <div>
+          <input type="Checkbox" name="movies" value="" onClick={(e) => this.clearFilters(e)}/>
+          <label>Clear Filters</label>
+        </div>
+        <div>
+          <input type="Checkbox" name="Action" value="" onClick={(e) => this.filterAction(e)} />
+          <label>Action</label>
+           <input type="Checkbox" name="Comedy" value="" onClick={(e) => this.filterComedy(e)}/>
+          <label>Comedy</label>
+           <input type="Checkbox" name="Animation" value="" onClick={(e) => this.filterAnimation(e)} />
+          <label>Animation</label>
+          <input type="Checkbox" name="Adventure" value="" onClick={(e) => this.filterMovies(e)}/>
+          <label>Adventure</label>
+        </div>
+
+
 
         <ul>
           {
@@ -49,6 +94,45 @@ class Movies extends React.Component  {
             }
           })
           }
+          {
+            data.media.map(movie=>{
+            if (movie.genre.includes('action') && this.state.genre ==='action') {
+            return <li key={movie.title}>{movie.title}-{movie.year}
+              <img src={movie.poster} alt="" />
+              </li>
+            }
+          })
+          }
+          {
+            data.media.map(movie=>{
+            if (movie.genre.includes('comedy') && this.state.genre ==='comedy') {
+            return <li key={movie.title}>{movie.title}-{movie.year}
+              <img src={movie.poster} alt="" />
+              </li>
+            }
+          })
+          }
+          {
+            data.media.map(movie=>{
+            if (movie.genre.includes('animation') && this.state.genre ==='animation') {
+            return <li key={movie.title}>{movie.title}-{movie.year}
+              <img src={movie.poster} alt="" />
+              </li>
+            }
+          })
+          }
+          {
+            data.media.map(movie=>{
+            if (movie.genre.includes('adventure') && this.state.genre ==='adventure') {
+            return <li key={movie.title}>{movie.title}-{movie.year}
+              <img src={movie.poster} alt="" />
+              </li>
+            }
+          })
+          }
+
+
+
       </ul>
     </div>
     )
